65b299046c7ea8d3d80fab52b1a6b416f70c6f12
Misael Lima <misael.alisson14@gmail.com>
2024-12-24 11:52:42 -0300

    initial commit
    set up project theme and colors

---
+++ app/src/main/java/com/pgmv/bandify/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.pgmv.bandify
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pgmv.bandify.ui.theme.BandifyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BandifyTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text(
+                        text = "Hello, Bandify!",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/pgmv/bandify/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.pgmv.bandify.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue80 = Color(0xFF0C356A)
+val Gold80 = Color(0xFFFFC436)
+val White = Color(0xFFFFFFFF)
+
+val Blue90 = Color(0xFF011535)
+val Grey10 = Color(0xFFF5F5F5)
+val Grey20 = Color(0xFFD9D9D9)
+val Grey60 = Color(0xFF737373)
+val Grey80 = Color(0xFF3f4446)
