3129c932da0dcd0960f47f18fe1b1bf5877341cb
Misael Lima <misael.alisson14@gmail.com>
2025-01-08 21:38:25 -0300

    fixed PerfilScreen and removed some unused imports

--- app/src/main/java/com/pgmv/bandify/ui/screen/PerfilScreen.kt
+++ app/src/main/java/com/pgmv/bandify/ui/screen/PerfilScreen.kt
@@ -1,22 +1,12 @@
 package com.pgmv.bandify.ui.screen
 
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import com.pgmv.bandify.database.DatabaseHelper
-import com.pgmv.bandify.domain.File
-import com.pgmv.bandify.domain.User
 
 @Composable
 fun PerfilScreen() {
     Text(text = "Perfil")
-    }
 }
 
 @Preview(
